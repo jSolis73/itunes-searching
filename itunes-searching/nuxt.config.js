50e6f784a29b8abb20db8debd7b05c4295376468
unknown <chez90@mail.ru>
2019-06-20 12:48:44 +0400

    add css in config, create app.styl

--- itunes-searching/nuxt.config.js
+++ itunes-searching/nuxt.config.js
@@ -10,7 +10,10 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {rel: 'styleshit',
+       href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'}
+
     ]
   },
   /*
@@ -26,6 +29,7 @@ module.exports = {
     ** Run ESLint on save
     */
   },
-  plugins: ['~plugins/vuetify.js']
+  plugins: ['~plugins/vuetify.js'],
+  css: ['~assets/app-styl']
 }
 
